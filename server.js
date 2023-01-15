1ef29f50cd876d854bd9db6f15d937d392994767
Claire Bourdon <cebourdon625@gmail.com>
2023-01-15 13:10:57 -0500

    files

---
+++ server.js
@@ -0,0 +1,26 @@
+//todo - Declare Variables
+const express = require("express");
+const app = express()
+const PORT = 8000
+const mongoose = require("mongoose")
+
+    //*Import functions/routes
+const connectDB = require("./config/database")
+const homeRoutes = require('./routes/home')
+
+require('dotenv').config({path: './config/.env'})
+
+//todo - Connect to Database
+connectDB()
+
+
+//todo - Set Middleware
+app.set("view engine", "ejs")
+app.set(express.static("public"))
+app.use(express.urlencoded({extended: true}))
+
+//todo - Set Routes
+app.use('/',homeRoutes)
+
+//todo - Start Server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
